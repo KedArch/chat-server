240e29446375994d3332c2f87a59510166cf0e80
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2020-07-03 14:36:34 +0200

    Modified argparse, added background/foreground + logging

--- chattext/chatserver/chatserver.py
+++ chattext/chatserver/chatserver.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import argparse
-import socket
-import threading
-import sys
 import os
+import sys
+import socket
 import select
+import signal
+import argparse
+import datetime
+import threading
 
 if os.name != "nt":
     import readline
@@ -15,19 +17,71 @@ basedir = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 class Server():
     def __init__(self):
-        self.parser = argparse.ArgumentParser(
-            description="Simple chat server.",
-            epilog="For now not taking any argument. Starts and logs"
-                   " to stdout. If given invalid argument, exit code "
-                   "2 will be returned.")
-        self.args = self.parser.parse_args()
         self.host = "0.0.0.0"
         self.port = 1111
         self.bufsiz = 4096
         self.code = "utf8"
         self.sname = "Serwer"
+        self.logtypes = ("SEP", "LOG", "CHAT")
+        self.logsep = 0
+        for i in self.logtypes:
+            if len(i) > self.logsep:
+                self.logsep = len(i)
+        signal.signal(signal.SIGTERM, self.exit)
+        signal.signal(signal.SIGINT, self.exit)
+
+    def exit(self, signum, frame=""):
+        try:
+            self.log("<<Logging ended at {}>>".format(
+                     str(datetime.datetime.now())),
+                     self.logtypes[0])
+            self.logfile.close()
+            self.server.close()
+        finally:
+            sys.exit(signum)
+
+    def log(self, text, logtype):
+        if self.args.log:
+            if logtype == self.logtypes[0]:
+                self.logfile.write(text + "\n")
+            else:
+                self.logfile.write(str(datetime.datetime.now()) + "|"
+                                   + logtype.ljust(self.logsep) + "|"
+                                   + text + "\n")
+            self.logfile.flush()
+        if self.args.foreground:
+            if logtype == self.logtypes[0]:
+                print(text)
+            else:
+                print(str(datetime.datetime.now()) + "|"
+                      + logtype.ljust(self.logsep) + "|"
+                      + text)
 
     def start(self):
+        if self.args.log:
+            dirname = os.path.dirname(self.args.log)
+            if os.path.exists(dirname) or dirname == "":
+                if os.path.exists(self.args.log):
+                    if self.args.append:
+                        answer = "1"
+                    elif self.args.overwrite:
+                        answer = "2"
+                    else:
+                        print("Given log file exists. What to do?\n"
+                              "1 - append\n2 - overwrite\n"
+                              "anything else - exit")
+                        answer = input("> ")
+                    if answer == "1":
+                        self.logfile = open(self.args.log, "a")
+                    elif answer == "2":
+                        self.logfile = open(self.args.log, "w")
+                    else:
+                        sys.exit(65)
+                else:
+                    self.logfile = open(self.args.log, "w")
+            else:
+                print("Given log directory does not exists. Exiting...")
+                sys.exit(65)
         self.clients = {}
         self.addresses = {}
         addr = (self.host, self.port)
@@ -37,18 +91,29 @@ class Server():
             self.server.bind(addr)
         except OSError:
             print(f"{addr[0]}:{addr[1]} is being used!")
-            sys.exit(1)
+            if self.args.logfile:
+                self.logfile.close()
+            sys.exit(66)
+        if not self.args.foreground:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
         self.server.listen()
-        print("Waiting for connections...")
+        self.log("<<Logging started at {}>>".format(
+                 str(datetime.datetime.now())),
+                 self.logtypes[0])
+        self.log("Waiting for connections...", self.logtypes[1])
         accepting_thread = threading.Thread(name="Accepting connections",
                                             target=self.accept_connections)
         accepting_thread.daemon = 1
         accepting_thread.start()
         try:
             accepting_thread.join()
-        except (KeyboardInterrupt, EOFError):
-            self.server.close()
-            sys.exit(0)
+        except EOFError:
+            self.exit(0)
 
     def send(self, data, client):
         client.sendall(bytes(data, self.code))
@@ -56,7 +121,7 @@ class Server():
     def broadcast(self, data, prefix=""):
         if prefix == "":
             prefix = self.sname + ": "
-        print(prefix + data)
+        self.log(prefix + data, self.logtypes[2])
         for sock in self.clients:
             try:
                 self.send(prefix + data, sock)
@@ -70,7 +135,7 @@ class Server():
     def accept_connections(self):
         while 1:
             client, address = self.server.accept()
-            print(f"{address[0]}:{address[1]} connected.")
+            self.log(f"{address[0]}:{address[1]} connected.", self.logtypes[1])
             self.send(
                 "Welcome on this server!\nFor start give "
                 "yourself a nickname so anyone can recognize you!",
@@ -83,7 +148,7 @@ class Server():
             handle_thread.start()
 
     def client_error(self, client, text, btext=""):
-        print(text)
+        self.log(text, self.logtypes[1])
         del self.addresses[client]
         if client in self.clients:
             del self.clients[client]
@@ -116,16 +181,17 @@ class Server():
                             continue
                         self.clients[client] = name
                         if oldname == "":
-                            print(
+                            self.log(
                                 f"{self.addresses[client][0]}"
                                 f":{self.addresses[client][1]}"
-                                f" took nickname {name}")
+                                f" took nickname {name}", self.logtypes[1])
                             msg = f"{name} connected to chat!"
                         else:
-                            print(
+                            self.log(
                                 f"{self.addresses[client][0]}"
                                 f":{self.addresses[client][1]}"
-                                f" changed nickname from {oldname} to {name}")
+                                f" changed nickname from {oldname} to {name}",
+                                self.logtypes[1])
                             msg = f"{oldname} changed nickname to {name}"
                         self.broadcast(msg)
                     elif data == ":h":
@@ -183,10 +249,28 @@ class Server():
                                   f"{self.addresses[client][0]}:"
                                   f"{self.addresses[client][1]}"
                                   " gave 3 times wrong command for nickname. "
-                                  "Either didn't understand or it was"
+                                  "Either they didn't understand or it was "
                                   "an attack.")
                 break
 
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description="Simple chat server.",
+            epilog="Error codes: 65 - log problems; 66 - socket problems")
+        parser.add_argument("-l", "--log", required=not {"-f", "--foreground"}
+                            & set(sys.argv) or {"-o", "--overwrite", "-a",
+                            "--append"} & set(sys.argv), help="log to file; "
+                            "required without -f")
+        parser.add_argument("-f", "--foreground", action="store_true",
+                            help="run in foreground instead")
+        loggroup = parser.add_mutually_exclusive_group()
+        loggroup.add_argument("-o", "--overwrite", action="store_true",
+                              help="overwrite log file")
+        loggroup.add_argument("-a", "--append", action="store_true",
+                              help="append to log file")
+        self.args = parser.parse_args()
+        self.start()
+
 
 if __name__ == "__main__":
-    Server().start()
+    Server().parse_args()
