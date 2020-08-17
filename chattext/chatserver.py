4cc9c3b96fabed8ab6601215651b2a1f9a511080
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2020-08-17 13:34:39 +0200

    Reformatting and tls/log options as argparse arguments

--- chattext/chatserver.py
+++ chattext/chatserver.py
@@ -25,11 +25,12 @@ class Server():
                 ":cn": "change nickname",
                 ":h": "help",
                 }
-        self.log = ("SEP", "LOG", "CHAT")
+        self.logtype = ("SEP", "LOG", "CHAT")
         self.logsep = 0
-        for i in self.log:
+        for i in self.logtype:
             if len(i) > self.logsep:
                 self.logsep = len(i)
+        self.context = None
         signal.signal(signal.SIGTERM, self.exit)
         signal.signal(signal.SIGINT, self.exit)
 
@@ -37,7 +38,7 @@ class Server():
         try:
             self.logging("<<Logging ended at {}>>".format(
                      str(datetime.datetime.now())),
-                     self.log[0])
+                     self.logtype[0])
             self.logfile.close()
             self.server.close()
         finally:
@@ -46,35 +47,52 @@ class Server():
     def logging(self, text, logtype):
         longtext = str(datetime.datetime.now()) + "|"\
                    + logtype.ljust(self.logsep) + "|"
-        if self.args.log:
-            if logtype == self.log[0]:
+        if self.log:
+            if logtype == self.logtype[0]:
                 self.logfile.write(text + "\n")
             else:
                 self.logfile.write(longtext + text + "\n")
             self.logfile.flush()
-        if self.args.foreground:
-            if logtype == self.log[0]:
+        if self.foreground:
+            if logtype == self.logtype[0]:
                 print(text)
             else:
                 print(longtext + text)
 
-    def start(self):
-        if self.args.key:
+    def start(self, foreground=False, log=None, overwrite=False, append=False,
+              key=None, cert=None, passwd=None):
+        if overwrite and append:
+            print("You must select 'overwrite' OR 'append'")
+            sys.exit(68)
+        if (overwrite or append) and not log:
+            print("'overwrite' and 'append' require 'log'")
+            sys.exit(65)
+        if not (foreground or log):
+            print("There must be 'foreground' or 'log' or both of them "
+                  "specified")
+            sys.exit(65)
+        if (key or cert) and not (key and cert):
+            print("There must be both 'key' and 'cert'")
+            sys.exit(67)
+        if passwd and not key:
+            print("'passwd' requires 'key'")
+            sys.exit(67)
+        if key:
             self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
             try:
-                self.context.load_cert_chain(certfile=self.args.cert,
-                                             keyfile=self.args.key,
-                                             password=self.args.password)
+                self.context.load_cert_chain(certfile=cert,
+                                             keyfile=key,
+                                             password=passwd)
             except ssl.SSLError:
                 print("Invalid cert/key/password")
                 sys.exit(67)
-        if self.args.log:
-            dirname = os.path.dirname(self.args.log)
+        if log:
+            dirname = os.path.dirname(log)
             if os.path.exists(dirname) or dirname == "":
-                if os.path.exists(self.args.log):
-                    if self.args.append:
+                if os.path.exists(log):
+                    if append:
                         answer = "1"
-                    elif self.args.overwrite:
+                    elif overwrite:
                         answer = "2"
                     else:
                         print("Given log file exists. What to do?\n"
@@ -82,13 +100,13 @@ class Server():
                               "anything else - exit")
                         answer = input("> ")
                     if answer == "1":
-                        self.logfile = open(self.args.log, "a")
+                        self.logfile = open(log, "a")
                     elif answer == "2":
-                        self.logfile = open(self.args.log, "w")
+                        self.logfile = open(log, "w")
                     else:
                         sys.exit(65)
                 else:
-                    self.logfile = open(self.args.log, "w")
+                    self.logfile = open(log, "w")
             else:
                 print("Given log directory does not exists. Exiting...")
                 sys.exit(65)
@@ -101,10 +119,10 @@ class Server():
             self.server.bind(addr)
         except OSError:
             print(f"{addr[0]}:{addr[1]} is being used!")
-            if self.args.logfile:
+            if self.logfile:
                 self.logfile.close()
             sys.exit(66)
-        if not self.args.foreground:
+        if not foreground:
             pid = os.fork()
             if pid > 0:
                 sys.exit(0)
@@ -114,11 +132,13 @@ class Server():
             pid = os.fork()
             if pid > 0:
                 sys.exit(0)
+        self.foreground = foreground
+        self.log = log
         self.server.listen()
         self.logging("<<Logging started at {}>>".format(
                  str(datetime.datetime.now())),
-                 self.log[0])
-        self.logging("Waiting for connections...", self.log[1])
+                 self.logtype[0])
+        self.logging("Waiting for connections...", self.logtype[1])
         accepting_thread = threading.Thread(
                 name="Accepting connections",
                 target=self.accept_connections)
@@ -135,7 +155,7 @@ class Server():
     def broadcast(self, data, prefix=""):
         if prefix == "":
             prefix = self.sname + ": "
-        self.logging(prefix + data, self.log[2])
+        self.logging(prefix + data, self.logtype[2])
         for sock in self.clients:
             try:
                 self.send(prefix + data, sock)
@@ -149,14 +169,15 @@ class Server():
     def accept_connections(self):
         while True:
             client, address = self.server.accept()
-            if self.args.key:
+            if self.context:
                 try:
                     client = self.context.wrap_socket(client, server_side=True)
                 except (ssl.SSLError, OSError):
                     self.logging(f"{address[0]}:{address[1]} SSL handshake "
-                                 "failed or disconnected", self.log[1])
+                                 "failed or disconnected", self.logtype[1])
                     continue
-            self.logging(f"{address[0]}:{address[1]} connected.", self.log[1])
+            self.logging(f"{address[0]}:{address[1]} connected.",
+                         self.logtype[1])
             self.send(self.welcome, client)
             self.addresses[client] = address
             handle_thread = threading.Thread(
@@ -166,7 +187,7 @@ class Server():
             handle_thread.start()
 
     def client_error(self, client, text, btext=""):
-        self.logging(text, self.log[1])
+        self.logging(text, self.logtype[1])
         del self.addresses[client]
         if client in self.clients:
             del self.clients[client]
@@ -207,14 +228,14 @@ class Server():
                             self.logging(
                                 f"{self.addresses[client][0]}"
                                 f":{self.addresses[client][1]}"
-                                f" took nickname {name}", self.log[1])
+                                f" took nickname {name}", self.logtype[1])
                             msg = f"{name} connected to chat!"
                         else:
                             self.logging(
                                 f"{self.addresses[client][0]}"
                                 f":{self.addresses[client][1]}"
                                 f" changed nickname from {oldname} to {name}",
-                                self.log[1])
+                                self.logtype[1])
                             msg = f"{oldname} changed nickname to {name}"
                         self.broadcast(msg)
                     elif data == ":h":
@@ -283,55 +304,58 @@ class Server():
                         " sent invalid character and was disconnected.")
                 break
 
-    def parse_args(self):
-        parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            description="Simple chat server intended for personal use",
-            epilog="Error codes and their "
-                   "element:\n65 - log\n66 - socket\n67 - certfile or "
-                   "keyfile (or its password)")
-        parser.add_argument(
-                "-f", "--foreground", action="store_true",
-                help="run in foreground instead")
-        logparser = parser.add_argument_group(
-                title="Logging",
-                description="Simple logging system")
-        logparser.add_argument(
-                "-l", "--log",
-                required=not {"-f", "--foreground"} &
-                set(sys.argv) or {"-o", "--overwrite", "-a", "--append"} &
-                set(sys.argv),
-                help="log to file; required without -f")
-        loggroup = logparser.add_mutually_exclusive_group()
-        loggroup.add_argument(
-                "-o", "--overwrite", action="store_true",
-                help="overwrite log file")
-        loggroup.add_argument(
-                "-a", "--append", action="store_true",
-                help="append to log file")
-        tlsgroup = parser.add_argument_group(
-                title="SSL/TLS", description="Existence of this parameters "
-                "enables TLS/SSL of best version available to both client "
-                "and server.\nHere's example of simple key and cert generation"
-                " on openssl:\nopenssl req -new -newkey rsa:2048 -sha256 -days"
-                "365 -nodes -x509 -keyout server.key -out server.crt\n"
-                "Refer to openssl's or other documentation for more info.")
-        tlsgroup.add_argument(
-                "-k", "--key",
-                required={"-c", "--cert", "-p", "--password"} & set(sys.argv),
-                help="private key location")
-        tlsgroup.add_argument(
-                "-c", "--cert",
-                required={"-k", "--key"} & set(sys.argv),
-                help="certificate location")
-        tlsgroup.add_argument(
-                "-p", "--password",
-                help="password to private key. WARNING! This option is "
-                "insecure. If not given will be asked for it at server start "
-                "if needed.")
-        self.args = parser.parse_args()
-        self.start()
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Simple chat server intended for personal use",
+        epilog="Error codes and their "
+               "element:\n65 - log\n66 - socket\n67 - certfile or "
+               "keyfile (or its password)\n68 - mutually exclusive arguments")
+    parser.add_argument(
+            "-f", "--foreground", action="store_true",
+            help="run in foreground instead")
+    logparser = parser.add_argument_group(
+            title="Logging",
+            description="Simple logging system")
+    logparser.add_argument(
+            "-l", "--log",
+            required=not {"-f", "--foreground"} &
+            set(sys.argv) or {"-o", "--overwrite", "-a", "--append"} &
+            set(sys.argv),
+            help="log to file; required without -f")
+    loggroup = logparser.add_mutually_exclusive_group()
+    loggroup.add_argument(
+            "-o", "--overwrite", action="store_true",
+            help="overwrite log file")
+    loggroup.add_argument(
+            "-a", "--append", action="store_true",
+            help="append to log file")
+    tlsgroup = parser.add_argument_group(
+            title="SSL/TLS", description="Existence of this parameters "
+            "enables TLS/SSL of best version available to both client "
+            "and server.\nHere's example of simple key and cert generation"
+            " on openssl:\nopenssl req -new -newkey rsa:2048 -sha256 -days"
+            "365 -nodes -x509 -keyout server.key -out server.crt\n"
+            "Refer to openssl's or other documentation for more info.")
+    tlsgroup.add_argument(
+            "-k", "--key",
+            required={"-c", "--cert", "-p", "--passwd"} & set(sys.argv),
+            help="private key location")
+    tlsgroup.add_argument(
+            "-c", "--cert",
+            required={"-k", "--key"} & set(sys.argv),
+            help="certificate location")
+    tlsgroup.add_argument(
+            "-p", "--passwd",
+            help="password to private key. WARNING! This option is "
+            "insecure. If not given will be asked for it at server start "
+            "if needed.")
+    args = parser.parse_args()
+    Server().start(
+            args.foreground, args.log, args.overwrite, args.append,
+            args.key, args.cert, args.passwd)
 
 
 if __name__ == "__main__":
-    Server().parse_args()
+    parse_args()
